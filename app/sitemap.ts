7882fc6a4c2aef33b6d347a5319766e55113149c
Krisna44556 <krisnajanuar16@gmail.com>
2026-07-13 02:05:20 +0700

    hapus file sitemaps dan robot

--- app/sitemap.ts
+++
@@ -1,8 +0,0 @@
-import { MetadataRoute } from 'next';
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    { url: 'https://indibizcimahi.com', lastModified: new Date() },
-    { url: 'https://indibizcimahi.com/produk-digital', lastModified: new Date() },
-    { url: 'https://indibizcimahi.com/artikel', lastModified: new Date() },
-  ];
-}
